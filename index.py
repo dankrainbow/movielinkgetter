0a059028fceea7bac15b513580692c868432f522
dank rainbow <79637386+dankrainbow@users.noreply.github.com>
2021-12-17 20:25:20 +0000

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+from googlesearch import search
+
+query = input("Movie name: ") ## ask what movie trying to search and save as variable called query
+queryjoined = "intitleof:indexof? mkv " + query ## add the bit before the query so it only returns urls with direct movie download
+
+with open('output.txt', 'a') as output:
+        for i in search(queryjoined, tld="co.in", num=10, stop=10, pause=2): ## search for the top 10 results
+            output.write(f"{i}\n") ## write link, add a new line
+            output.close ## close the file
