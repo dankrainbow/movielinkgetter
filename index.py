10ff8fa0f264edb5888c3ba0ca9a393dc018a5f7
dankrainbow <79637386+dankrainbow@users.noreply.github.com>
2021-12-17 20:30:59 +0000

    Update index.py

--- index.py
+++ index.py
@@ -6,4 +6,5 @@ queryjoined = "intitleof:indexof? mkv " + query ## add the bit before the query 
 with open('output.txt', 'a') as output:
         for i in search(queryjoined, tld="co.in", num=10, stop=10, pause=2): ## search for the top 10 results
             output.write(f"{i}\n") ## write link, add a new line
+            print(f"1 link added to txt : {i}")
             output.close ## close the file
